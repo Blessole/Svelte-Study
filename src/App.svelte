1073f4e1c81baf266a9614c3a8cfaf7b3a1021b7
Blessole <sorihs1210@gmail.com>
2025-01-03 17:30:55 +0900

    event 및 reactivity 객체 활용하기

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,38 @@
 <script>
-  import logo1 from "./assets/svelte.svg";
-  let logo2 = "/vite.svg";
-  let title = "영화제목1";
-  let year = 2024;
-  let category = "로맨스";
-  let textRed = "color: red;";
-
   const data = [
     {
       title: "고질라 X 콩 : 뉴 엠파이어",
       year: 2024,
       category: "어드벤처, 액션, SF",
+      likeCount: 0,
     },
     {
       title: "듄: 파트2",
       year: 2024,
       category: "액션",
+      likeCount: 0,
     },
   ];
+
+  let likeCount = 0; // 좋아요 수를 저장할 변수
+  const handleLike = (i) => {
+    data[i].likeCount += 1;
+    console.log(likeCount);
+  };
 </script>
 
 <main>
   <h1>영화정보</h1>
-  <img src={logo1} alt="" />
-  <img src={logo2} alt="" />
-  <div>
-    <h3 class="bg-yellow" style={textRed}>{title}</h3>
-    <p>개봉 : {year}</p>
-    <p>장르 : {category}</p>
-    <p>{"hello".toUpperCase()}</p>
-  </div>
   {#each data as movie, i}
     <!-- content here -->
-    <h3 class="bg-yellow" style={textRed}>{i + 1} : {movie.title}</h3>
+    <h3 class="bg-yellow">{movie.title}</h3>
     <p>개봉 : {movie.year}</p>
     <p>장르 : {movie.category}</p>
+    <button
+      onclick={() => {
+        handleLike(i);
+      }}>좋아요 {movie.likeCount}</button
+    >
   {/each}
 </main>
 
@@ -42,5 +40,6 @@
   .bg-yellow {
     background: gold;
     padding: 10px;
+    color: #333;
   }
 </style>
