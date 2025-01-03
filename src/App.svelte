cf6dea0bbd5492dbe04d9df8ac626b51af1d384e
Blessole <sorihs1210@gmail.com>
2025-01-03 17:55:54 +0900

    조건문으로 동적 콘텐츠(Modal)추가

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,8 @@
       category: "어린이, 로맨스",
       likeCount: 0,
       imgUrl: "/images/elemental_poster.jpg",
+      story:
+        "불, 물, 땅, 공기, 각양각색의 원소들이 함께 살아가는 엘리멘트 시티, 열정적인 불의 여자 앰버와 감수성 풍부한 물의 남자 웨이드가 서로에게 끌린다.",
     },
     {
       title: "하얼빈",
@@ -13,6 +15,8 @@
       category: "액션, 범죄",
       likeCount: 0,
       imgUrl: "/images/haerbin_poster.jpg",
+      story:
+        "1908년 함경북도 신아산에서 안중근이 이끄는 독립군들은 일본군과의 전투에서 큰 승리를 거둔다. 대한의군 참모중장 안중근은 만국공법에 따라 전쟁포로인 일본인들을 풀어주게 되고, 이 사건으로 인해 독립군 사이에서는 안중근에 대한 의심과 함께 균열이 일기 시작한다. 1년 후, 블라디보스토크에는 안중근을 비롯해 우덕순, 김상현, 공부인, 최재형, 이창섭 등 빼앗긴 나라를 되찾기 위해 마음을 함께하는 이들이 모이게 된다. 이토 히로부미가 러시아와 협상을 위해 하얼빈으로 향한다는 소식을 접한 안중근과 독립군들은 하얼빈으로 향하고, 내부에서 새어 나간 이들의 작전 내용을 입수한 일본군들의 추격이 시작되는데… 하얼빈을 향한 단 하나의 목표, 늙은 늑대를 처단하라",
     },
     {
       title: "알라딘",
@@ -20,6 +24,8 @@
       category: "어린이, 뮤지컬",
       likeCount: 0,
       imgUrl: "/images/aladin_poster.jpg",
+      story:
+        "사막 속에 자리한 아그라바 왕국, 좀도둑질로 생계를 유지하는 알라딘은 우연히 마주친 공주 자스민에게 신분도 모른 채 마음을 빼앗긴다. 한편, 사악한 마법사 자파는 왕국을 손에 넣기 위해 마법의 동굴 속 어떤 램프를 손에 넣으려 하고, 동굴에 접근할 수 있는 맑은 영혼을 가진 알라딘을 이용해 가져오려는 계획을 세운다. 의도치 않게 램프는 알라딘의 차지가 되고, 알라딘은 램프의 요정 지니가 들어주는 소원 덕에 왕자의 모습으로 탈바꿈한다.",
     },
   ];
 
@@ -28,6 +34,8 @@
     data[i].likeCount += 1;
     console.log(likeCount);
   };
+
+  let isModal = false; // 모달창 변수 추가가
 </script>
 
 <main class="container">
@@ -46,10 +54,30 @@
             handleLike(i);
           }}>좋아요 {movie.likeCount}</button
         >
+        <button
+          class="btn btn-primary"
+          onclick={() => {
+            isModal = true;
+          }}>상세보기</button
+        >
       </div>
     </div>
   {/each}
 </main>
+
+{#if isModal}
+  <div class="modal">
+    <div class="inner">
+      <h3>영화 상세 정보</h3>
+      <button
+        class="btn-close"
+        onclick={() => {
+          isModal = false;
+        }}>닫기</button
+      >
+    </div>
+  </div>
+{/if}
 
 <style>
   .bg-yellow {
@@ -77,5 +105,25 @@
 
   .item .info {
     width: 100%;
+  }
+
+  /* Modal 관련 */
+  .modal {
+    background: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal .inner {
+    background-color: #fff;
+    width: 80%;
+    padding: 20px;
+    border-radius: 10px;
   }
 </style>
