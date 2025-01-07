b5a5cca471ee73044558b76a67867bc2d9da341e
Blessole <sorihs1210@gmail.com>
2025-01-07 20:04:29 +0900

    event 컴포넌트로 분리하기

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
-  import { fade, fly } from "svelte/transition";
   import data from "./lib/movies";
   import Modal from "./lib/components/Modal.svelte";
   import Movies from "./lib/components/Movies.svelte";
+  import Event from "./lib/components/Event.svelte";
 
   let likeCount = 0; // 좋아요 수를 저장할 변수
   const handleLike = (i) => {
@@ -27,20 +27,9 @@
 
 <!-- <div class={isEvent ? "event show" : "event"}> -->
 {#if isEvent}
-  <div class="event show" in:fly={{ y: -400, duration: 1000 }} out:fade>
-    <p>NETPLIX 강렬한 운명의 드라마, 경기크리처</p>
-    <button
-      onclick={() => {
-        isEvent = false;
-      }}>X</button
-    >
-  </div>
+  <Event bind:isEvent />
 {/if}
-<button
-  onclick={() => {
-    isEvent = true;
-  }}>이벤트 창 나타나기</button
->
+
 <Movies {data} bind:isModal {handleMovieNumber} {handleLike} />
 
 {#if isModal}
@@ -52,30 +41,4 @@
 {/if}
 
 <style>
-  .event {
-    width: 100%;
-    background-color: #666;
-    padding: 5px 1em;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: white;
-    text-align: center;
-    margin-bottom: 1em;
-
-    /* 창이 보이지 않게 */
-    max-height: 0;
-    opacity: 0;
-    overflow: hidden;
-    transition: all 0.4s;
-  }
-
-  /* 기본 : 창이 보이게 */
-  .show {
-    opacity: 1;
-    max-height: 100px;
-  }
-  .event button {
-    padding: 2px;
-  }
 </style>
---
+++ src/lib/components/Event.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { fade, fly } from "svelte/transition";
+    export let isEvent;
+</script>
+
+<div class="event show" in:fly={{ y: -400, duration: 1000 }} out:fade>
+    <p>NETPLIX 강렬한 운명의 드라마, 경기크리처</p>
+    <button
+        onclick={() => {
+            isEvent = false;
+        }}>X</button
+    >
+</div>
+
+<style>
+    .event {
+        width: 100%;
+        background-color: #666;
+        padding: 5px 1em;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: white;
+        text-align: center;
+        margin-bottom: 1em;
+
+        /* 창이 보이지 않게 */
+        max-height: 0;
+        opacity: 0;
+        overflow: hidden;
+        transition: all 0.4s;
+    }
+
+    /* 기본 : 창이 보이게 */
+    .show {
+        opacity: 1;
+        max-height: 100px;
+    }
+    .event button {
+        padding: 2px;
+    }
+</style>
