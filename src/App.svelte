6e4ba0376ba89be8259df04276cbdffbf6b9c05c
Blessole <sorihs1210@gmail.com>
2025-01-07 19:45:03 +0900

    영화 목록 부분 컴포넌트로 수정

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
   import data from "./lib/movies";
   import Modal from "./lib/components/Modal.svelte";
+  import Movies from "./lib/components/Movies.svelte";
 
   let likeCount = 0; // 좋아요 수를 저장할 변수
   const handleLike = (i) => {
     data[i].likeCount += 1;
-    console.log(likeCount);
   };
 
   let isModal = false; // 모달창 변수 추가
@@ -15,35 +15,13 @@
   const closeModal = () => {
     isModal = false;
   };
+
+  const handleMovieNumber = (i) => {
+    selectedMovie = i;
+  };
 </script>
 
-<main class="container">
-  <h1>영화정보</h1>
-  {#each data as movie, i}
-    <div class="item">
-      <figure>
-        <img src={movie.imgUrl} alt={movie.title} />
-      </figure>
-      <div class="info">
-        <h3 class="bg-yellow">{movie.title}</h3>
-        <p>개봉 : {movie.year}</p>
-        <p>장르 : {movie.category}</p>
-        <button
-          onclick={() => {
-            handleLike(i);
-          }}>좋아요 {movie.likeCount}</button
-        >
-        <button
-          class="btn btn-primary"
-          onclick={() => {
-            isModal = true;
-            selectedMovie = i;
-          }}>상세보기</button
-        >
-      </div>
-    </div>
-  {/each}
-</main>
+<Movies {data} bind:isModal {handleMovieNumber} {handleLike} />
 
 {#if isModal}
   <!-- 단방향props바인딩 : <자식컴포넌트이름 props명={전달값|변수명} /> -->
@@ -54,30 +32,4 @@
 {/if}
 
 <style>
-  .bg-yellow {
-    background: gold;
-    padding: 10px;
-    color: #333;
-  }
-
-  .item {
-    width: 100%;
-    border: 1px solid #ccc;
-    display: flex;
-    margin-bottom: 20px;
-    padding: 1rem;
-  }
-
-  .item figure {
-    width: 30%;
-    margin-right: 1rem;
-  }
-
-  .item img {
-    width: 100%;
-  }
-
-  .item .info {
-    width: 100%;
-  }
 </style>
---
+++ src/lib/components/Movies.svelte
@@ -0,0 +1,67 @@
+<script>
+    export let data = []; // 기본 값이 배열형식이라 []
+    export let isModal = false;
+    export let handleLike;
+    // export let selectedMovie = 0; // 값에 바로 바인딩
+    export let handleMovieNumber; // 함수를 활용해서 바인딩
+
+    // TODO : 좋아요 값이 바로 업데이트가 안되는 문제가 있음
+</script>
+
+<main class="container">
+    <h1>영화정보</h1>
+    {#each data as movie, i}
+        <div class="item">
+            <figure>
+                <img src={movie.imgUrl} alt={movie.title} />
+            </figure>
+            <div class="info">
+                <h3 class="bg-yellow">{movie.title}</h3>
+                <p>개봉 : {movie.year}</p>
+                <p>장르 : {movie.category}</p>
+                <button
+                    onclick={() => {
+                        handleLike(i);
+                    }}>좋아요 {movie.likeCount}</button
+                >
+                <button
+                    class="btn btn-primary"
+                    onclick={() => {
+                        isModal = true;
+                        // selectedMovie = i;
+                        handleMovieNumber(i);
+                    }}>상세보기</button
+                >
+            </div>
+        </div>
+    {/each}
+</main>
+
+<style>
+    .bg-yellow {
+        background: gold;
+        padding: 10px;
+        color: #333;
+    }
+
+    .item {
+        width: 100%;
+        border: 1px solid #ccc;
+        display: flex;
+        margin-bottom: 20px;
+        padding: 1rem;
+    }
+
+    .item figure {
+        width: 30%;
+        margin-right: 1rem;
+    }
+
+    .item img {
+        width: 100%;
+    }
+
+    .item .info {
+        width: 100%;
+    }
+</style>
